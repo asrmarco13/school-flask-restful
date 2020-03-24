501d9ca068582b258a02a117185722e88cb05745
Marco Orfei <marcoasrorfei@gmail.com>
2020-03-24 18:06:32 +0100

    Update schools service

--- resources/schools_list.py
+++ resources/schools_list.py
@@ -1,9 +1,19 @@
 from flask_restplus import Resource
 from models.school import SchoolModel
+from flask_jwt_extended import jwt_optional, get_jwt_identity
 
 
 class SchoolsList(Resource):
+    @jwt_optional
     def get(self):
+        username = get_jwt_identity()
+        schools = [school.json() for school in SchoolModel.query.all()]
+        if username:
+            return {
+                "schools": schools
+            }, 200
+
         return {
-            "schools": [school.json() for school in SchoolModel.query.all()]
-        }
+            "schools": [school['name'] for school in schools],
+            "message": "More data available if you log in"
+        }, 200
